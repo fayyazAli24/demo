56e528fa0eb103a1b2fecba7b39d9efd56f96f3d
Muhammad Fayyaz Ali <fayyaz.ali@covalent.global>
2025-07-09 22:44:23 +0500

    world

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import { createServer } from 'node:http';
 
 const server = createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello world');
+  res.end('Hello world guys');
 });
 
 // starts a simple http server locally on port 3000
