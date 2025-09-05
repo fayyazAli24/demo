eaf0802436a1d147bdd9f3b5c9596056f4cd69bc
Muhammad Fayyaz Ali <fayyaz.ali@covalent.global>
2025-09-05 16:24:14 +0500

    demo

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const winston = require('winston');
+const app = express();
+const port = 3000;
+
 
 app.get('/', (req, res) => {
-  res.send('hello world gays')
-})
+ 
+  res.send('hello world gays');
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-}) 
+  logger.info(`App listening on port ${port}`);
+});
